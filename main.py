2f01faf28668ff401276c26ecf0f82373ed323b1
Himanshu041430 <Himanshudctr@gmail.com>
2025-11-27 17:25:53 -0500

    feature/framework commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("this is Etl Automation Framwork")
 print("Now i am in release_branch1" )
+
+
+# team1 :- create new branch and add framework in it
+# team2 :- create one feature branch and add test cases for table5
+# Fetch :-  download new code from remote for all branch ( including main branch)(keep in the temporary memory)
